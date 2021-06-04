8d4a35b31c182935dd9c4b7ced69fc96512a92b6
Kellen Mace <kellen.mace@gmail.com>
2021-06-04 12:09:41 -0400

    Checkpoint 3 - Single category page

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -5,9 +5,6 @@ import { client } from "../../lib/apolloClient";
 import Layout from "../../components/Layout";
 import PostsList from "../../components/PostsList";
 
-// Dummy data
-import { category } from "../../dummy-data";
-
 export default function SingleCategory({ category }) {
   return (
     <Layout>
@@ -24,8 +21,40 @@ export function getStaticPaths() {
   };
 }
 
+const GET_CATEGORY = gql`
+  query getCategory($slugId: ID!, $slugName: String!) {
+    category(id: $slugId, idType: SLUG) {
+      name
+      posts(first: 18, after: null, where: { categoryName: $slugName }) {
+        nodes {
+          databaseId
+          title
+          excerpt
+          uri
+          featuredImage {
+            node {
+              sourceUrl
+              altText
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export async function getStaticProps(context) {
   const { slug } = context.params;
+
+  const response = await client.query({
+    query: GET_CATEGORY,
+    variables: {
+      slugId: slug,
+      slugName: slug,
+    },
+  });
+
+  const category = response?.data?.category;
 
   if (!category) {
     return { notFound: true };
